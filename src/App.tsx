bc3461e63514a7178f51ed18c360635058a031d3
Sakaar Puri <sakaarpuri@mac.home>
2026-03-02 00:16:20 +0000

    Add soft-retro design preview page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import Layout from './components/Layout';
 import MyFinds from './pages/MyFinds';
 import AuthCallback from './pages/AuthCallback';
 import DesignSoftPreview from './pages/DesignSoftPreview';
+import DesignSoftRetroPreview from './pages/DesignSoftRetroPreview';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="design/soft" element={<DesignSoftPreview />} />
+        <Route path="design/soft-retro" element={<DesignSoftRetroPreview />} />
         <Route element={<Layout />}>
           <Route index element={<MyFinds />} />
           <Route path="auth/callback" element={<AuthCallback />} />
